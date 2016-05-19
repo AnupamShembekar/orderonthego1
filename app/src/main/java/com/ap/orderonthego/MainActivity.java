714462d786fc2ab7c10ff2ce0bc8b946aeec6003
Pranav Dixit <pranavdixit20@gmail.com>
2016-05-19 20:40:25 +0530

    work on db

--- app/src/main/java/com/ap/orderonthego/MainActivity.java
+++ app/src/main/java/com/ap/orderonthego/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,15 +34,22 @@ public class MainActivity extends Activity implements View.OnClickListener {
         btn.setOnClickListener(this);
 
         dtc= new Main_courseDB(this);
-//       dtc.onCreate(sdb);
-        sdb=dtc.getWritableDatabase();
-        dtc.sinsertDB();
-        dtc.dinsertDB();
-        dtc.ninsertDB();
-        dtc.oinsertDB();
-        dtc.vinsertDB();
-         dtc.AadminDB();
-        dtc.tinsert();
+
+        int h = dtc.cheak();
+        if(h==1)
+        {
+            dtc.sinsertDB();
+            dtc.dinsertDB();
+            dtc.ninsertDB();
+            dtc.oinsertDB();
+            dtc.vinsertDB();
+            dtc.AadminDB();
+            dtc.tinsert();
+        }
+        else
+        {
+            Log.d("database operations", "Data exits");
+        }
 
 
     }
